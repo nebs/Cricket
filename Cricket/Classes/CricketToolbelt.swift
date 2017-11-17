00d8d9ae1333a13f80a8d338dddd552a80743b73
Nebojsa Petrovic <nebspetrovic@gmail.com>
2017-11-16 20:33:55 -0500

    Fix tab bar top view detection
    If there's a tab bar we want to route the modal on top of it to make
    sure cricket shows on top.

--- Cricket/Classes/CricketToolbelt.swift
+++ Cricket/Classes/CricketToolbelt.swift
@@ -15,10 +15,8 @@ class CricketToolbelt {
         if let navigationController = controller as? UINavigationController {
             return topViewController(controller: navigationController.visibleViewController)
         }
-        if let tabController = controller as? UITabBarController {
-            if let selected = tabController.selectedViewController {
-                return topViewController(controller: selected)
-            }
+        if controller is UITabBarController {
+            return controller
         }
         if let presented = controller?.presentedViewController {
             return topViewController(controller: presented)
